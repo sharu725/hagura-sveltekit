f76e8aa0a1c036eefe4f7e4f0846d56414234bcb
sharathdt <sharath.dt@gmail.com>
2021-05-30 20:33:44 +0530

    add basic SEO

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,3 +6,11 @@
 <div class="body-wrapper max-w-screen-md mx-auto">
   <slot />
 </div>
+
+<svelte:head>
+  <title>Hagura - Light</title>
+  <meta
+    name="description"
+    content="Hagura is a light-weight theme/template built for sveltekit"
+  />
+</svelte:head>
