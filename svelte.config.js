b0c5efa65976b135c07684643e6103d38a65c0c5
sharathdt <sharath.dt@gmail.com>
2022-02-11 07:24:30 +0530

    update adapter to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 import { mdsvexConfig } from "./mdsvex.config.js";
-import adapter from "@sveltejs/adapter-netlify";
+// import adapter from "@sveltejs/adapter-netlify";
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
